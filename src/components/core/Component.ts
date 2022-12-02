c266215bd4c7a951e698d12955ab88a575f5b56b
yeeed711 <rladpwl910@naver.com>
2022-12-02 22:44:28 +0900

    ✨ feat: core component 구현

---
+++ src/components/core/Component.ts
@@ -0,0 +1,121 @@
+import { ConvertTemplateToComponent } from '@utils/dom'
+
+export default abstract class Component<StateType> {
+  node: Element
+  initalState: StateType
+  renderStateKey: Set<string>
+  subscribers: Set<any>
+  isNeedRender: boolean
+  isNeedUpdate: boolean
+  constructor({
+    node,
+    initalState,
+    renderStateKey = []
+  }: IComponentProps<StateType>) {
+    this.node = node
+    this.initalState = initalState
+    this.renderStateKey = new Set(renderStateKey)
+    this.subscribers = new Set([])
+    this.isNeedRender = false
+    this.isNeedUpdate = false
+
+    this.setup()
+    this.render()
+  }
+
+  setup(): void {
+    return
+  }
+
+  render(): void {
+    ConvertTemplateToComponent.call(this)
+    this.setEvent()
+    this.renderChildComponent()
+  }
+
+  updater(): void {
+    if (!this.isNeedRender) {
+      this.renderChildComponent()
+      return
+    }
+
+    this.clearEvent()
+    this.render()
+  }
+
+  renderChildComponent(): void {
+    return
+  }
+
+  setEvent(): void {
+    return
+  }
+
+  clearEvent(): void {
+    return
+  }
+
+  template(): string {
+    return ''
+  }
+
+  subscribe(...subscribers: any[]): void {
+    subscribers.forEach(subscriber => {
+      this.subscribers.add(subscriber)
+    })
+  }
+
+  notify(newState: any): void {
+    const subscribers = Array.from(this.subscribers)
+
+    subscribers.forEach(subscriber => {
+      subscriber.setState(newState)
+      subscriber.updater()
+      subscriber.isNeedRender = false
+    })
+  }
+
+  setState(newState: any): void {
+    this.checkUpdataState(newState)
+
+    if (!this.isNeedUpdate) {
+      return
+    }
+
+    this.refleceNeedRender(newState)
+    this.reflectState(newState)
+    this.notify(newState)
+  }
+
+  checkUpdataState(newState: any): void {
+    const currentState = JSON.stringify({ ...this.initalState })
+    const nextState = JSON.stringify({ ...this.initalState, ...newState })
+
+    this.isNeedUpdate = currentState !== nextState
+  }
+
+  refleceNeedRender(newState: any): void {
+    const updatedStateKeys = Object.keys(newState)
+    const renderStateKey = Array.from(this.renderStateKey)
+
+    const state = updatedStateKeys.filter(key => renderStateKey.includes(key))
+    this.isNeedRender = state.length === 0
+  }
+
+  reflectState(newState: any): void {
+    const updatedStateKeys = Object.keys(newState)
+
+    updatedStateKeys.forEach(key => {
+      const stateKey = key as keyof StateType
+      this.initalState[stateKey] = newState[
+        stateKey
+      ] as StateType[keyof StateType]
+    })
+  }
+}
+
+export interface IComponentProps<StateType> {
+  node: Element
+  initalState: StateType | null
+  renderStateKey?: string[]
+}
