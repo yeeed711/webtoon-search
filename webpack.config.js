8380dc0ce95a3ecf25e4515d3bf93535f248b0b4
yeeed711 <rladpwl910@naver.com>
2022-12-02 22:43:50 +0900

    🔧 config: webpack.config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
-const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const tsConfigPath = path.resolve(__dirname, './tsconfig.json');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const tsConfigPath = path.resolve(__dirname, './tsconfig.json')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: ['./src/main.ts', './src/scss/index.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].bundle.js',
-    clean: true,
+    clean: true
   },
   resolve: {
     extensions: ['.ts', '.js'],
     plugins: [
       new TsconfigPathsPlugin({
-        configFile: tsConfigPath,
-      }),
-    ],
+        configFile: tsConfigPath
+      })
+    ]
   },
   module: {
     rules: [
       {
-        test: /\\.(png|jpg|gif|svg)$/i,
+        test: /\.(png|jpg|gif|svg)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/images/[name][ext]',
-        },
+          filename: 'assets/images/[name][ext]'
+        }
       },
+
       {
-        test: /\\.ts$/,
+        test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
-        test: /\\.s[ac]ss$/i,
+        test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
-        test: /\\.m?js$/,
+        test: /\.m?js$/,
         include: [path.resolve(__dirname, 'src')],
         exclude: /node_modules/,
         use: {
@@ -51,33 +52,33 @@ module.exports = {
                 '@babel/preset-env',
                 {
                   useBuiltIns: 'usage',
-                  corejs: 3,
-                },
+                  corejs: 3
+                }
               ],
-              '@babel/preset-typescript',
+              '@babel/preset-typescript'
             ],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          },
-        },
-      },
-    ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
   },
   devtool: 'inline-source-map',
   devServer: {
     client: {
-      logging: 'none',
-    },
+      logging: 'none'
+    }
   },
   plugins: [
     new HtmlPlugin({
       template: './public/index.html',
-      favicon: path.resolve(__dirname, 'public/favicon.ico'),
+      favicon: path.resolve(__dirname, 'public/favicon.ico')
     }),
     new CopyPlugin({
-      patterns: [{ from: 'public/favicon.ico' }],
+      patterns: [{ from: 'public/favicon.ico' }]
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/css/style.[contenthash].css',
-    }),
-  ],
-};
+      filename: 'assets/css/style.[contenthash].css'
+    })
+  ]
+}
