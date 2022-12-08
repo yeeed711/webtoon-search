a115f9a0af34e1ff0bdcec138188df20b98b4f6c
yeeed711 <rladpwl910@naver.com>
2022-12-08 18:57:27 +0900

    🐛 fix: get 요청시 undefined 반환, 기본값 추가

--- src/components/search/SearchItemInfo/index.ts
+++ src/components/search/SearchItemInfo/index.ts
@@ -8,12 +8,12 @@ export default class SearchItemInfo extends Component<ISearchItemInfoState> {
     <div class='${isModalVisiable ? `item_info` : 'hide'}' >
       <div class='modal'>
         <button>X</button> 
-        <a href='${selectedItem.url}' target='_blank'>
-          <img src='${selectedItem.img}' />
+        <a href='${selectedItem.url || ''}' target='_blank'>
+          <img src='${selectedItem.img || ''}' />
         </a>
-        <p>웹툰 제목: ${selectedItem.title}</p>
-        <p>작가: ${selectedItem.author}</p>
-        <p>연재 사이트: ${selectedItem.service}</p>
+        <p>웹툰 제목: ${selectedItem.title || ''}</p>
+        <p>작가: ${selectedItem.author || ''}</p>
+        <p>연재 사이트: ${selectedItem.service || ''}</p>
       </div>
     </div>
     `
