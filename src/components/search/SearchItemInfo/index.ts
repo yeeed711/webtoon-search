264e383d3cbfbc127aee76ace6a7f03facc95e5b
yeeed711 <rladpwl910@naver.com>
2022-12-09 02:04:19 +0900

    ♻️ refactor: isModalVisiable 부분 바깥영역에서 해결

--- src/components/search/SearchItemInfo/index.ts
+++ src/components/search/SearchItemInfo/index.ts
@@ -5,16 +5,24 @@ export default class SearchItemInfo extends Component<ISearchItemInfoState> {
   template(): string {
     const { isModalVisiable, selectedItem } = this.initalState
     return `
-    <div class='${isModalVisiable ? `item_info` : 'hide'}' >
-      <div class='modal'>
-        <button>X</button> 
-        <a href='${selectedItem.url || ''}' target='_blank'>
-          <img src='${selectedItem.img || ''}' />
-        </a>
-        <p>웹툰 제목: ${selectedItem.title || ''}</p>
-        <p>작가: ${selectedItem.author || ''}</p>
-        <p>연재 사이트: ${selectedItem.service || ''}</p>
+    <div>
+    ${
+      isModalVisiable
+        ? `
+      <div class='item_info' >
+        <div class='modal'>
+          <button>X</button> 
+          <a href='${selectedItem.url}' target='_blank'>
+            <img src='${selectedItem.img}' />
+          </a>
+          <p>웹툰 제목: ${selectedItem.title}</p>
+          <p>작가: ${selectedItem.author}</p>
+          <p>연재 사이트: ${selectedItem.service}</p>
+        </div>
       </div>
+      `
+        : ''
+    }
     </div>
     `
   }
