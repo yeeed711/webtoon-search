8a3590e08eaf7a24b9852967cdc0c4dace2152bf
yeeed711 <rladpwl910@naver.com>
2022-12-09 17:14:37 +0900

    💄 ui: SearchItemInfo style 수정

--- src/components/search/SearchItemInfo/index.ts
+++ src/components/search/SearchItemInfo/index.ts
@@ -1,23 +1,26 @@
 import { Component } from '@components'
 import type { IItem } from '../SearchResult'
+import styles from './SearchItemInfo.module.scss'
 
 export default class SearchItemInfo extends Component<ISearchItemInfoState> {
   template(): string {
     const { isModalVisiable, selectedItem } = this.initalState
+    const { item_info, modal, info_text_cont } = styles
     return `
     <div>
     ${
       isModalVisiable
         ? `
-      <div class='item_info' >
-        <div class='modal'>
-          <button>X</button> 
-          <a href='${selectedItem.url}' target='_blank'>
-            <img src='${selectedItem.img}' />
-          </a>
-          <p>웹툰 제목: ${selectedItem.title}</p>
-          <p>작가: ${selectedItem.author}</p>
-          <p>연재 사이트: ${selectedItem.service}</p>
+      <div class='${item_info} item_info' >
+        <div
+          class='${modal}'
+          style='background-image:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${selectedItem.img});'>
+          <button class="material-icons">cancel</button>
+          <div class='${info_text_cont}'>
+            <p>${selectedItem.title} <a href='${selectedItem.url}' target='_blank'>웹사이트</a></p>
+            <p>${selectedItem.author}</p>
+            <p>${selectedItem.service} 연재</p>
+          </div>
         </div>
       </div>
       `
@@ -32,3 +35,4 @@ interface ISearchItemInfoState {
   selectedItem: IItem
   isModalVisiable: boolean
 }
+//<img src='${selectedItem.img}' />
